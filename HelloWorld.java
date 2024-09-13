f8197bdf727285b0be11f1358f9aa3ae015c5ee7
Kayla Moore <kmoore97@email.cpcc.edu>
2024-09-12 21:28:50 -0400

    Create HelloWorld Program
    Hello World Program

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+/*
+This is a starter program for GitHub. 
+*/
+public class HelloWorld //class name. 
+{
+public static void main(String[] args)//this is the main method. 
+   {
+   //here I create a scanner class for keyboard input. 
+     Scanner keyboard = new Scanner(System.in);
+     System.out.println("Hello World.");
+   }
+ }
